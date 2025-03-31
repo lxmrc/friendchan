b4a34a9e5fe52da16ac7736d8f58e1ee51687b52
Alex Mercea <alex@lxmrc.com>
2025-03-31 13:27:52 +1100

    Update styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_31_020910) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_31_132550) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "topic_id", null: false
+    t.text "content"
+    t.string "image_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_31_020910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "topics"
 end
